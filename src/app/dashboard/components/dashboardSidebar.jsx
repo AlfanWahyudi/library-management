254e974a73daa40efd9632858e6d38f107cc8689
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-07-02 14:10:28 +0700

    UPD: tidy up layout for responsive sidebar

--- src/app/dashboard/components/dashboardSidebar.jsx
+++ src/app/dashboard/components/dashboardSidebar.jsx
@@ -1,71 +1,192 @@
-"use client"
+"use client";
 
 import { Button } from "@/components/ui/button";
 
-import { Sidebar, SidebarContent, useSidebar, SidebarHeader, SidebarGroup, SidebarFooter, SidebarMenu, SidebarMenuItem, SidebarMenuButton } from "@/components/ui/sidebar";
+import {
+	Sidebar,
+	SidebarContent,
+	useSidebar,
+	SidebarHeader,
+	SidebarGroup,
+	SidebarFooter,
+	SidebarMenu,
+	SidebarMenuItem,
+	SidebarMenuButton,
+	SidebarMenuSub,
+	SidebarMenuSubItem,
+	SidebarMenuSubButton,
+	SidebarMenuAction,
+} from "@/components/ui/sidebar";
+import {
+	Collapsible,
+	CollapsibleContent,
+	CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+	LayoutDashboard,
+	Book,
+	Users,
+	Scale,
+	LogOut,
+	ChevronDown,
+	CircleUserRound,
+	SquareLibrary,
+	MoreHorizontal,
+} from "lucide-react";
+import { Avatar } from "@/components/ui/avatar";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 
-const items = [
-  {
-    title: "Dashboard",
-    url: "#"
-  },
-  {
-    title: "Buku",
-    url: "#"
-  },
-  {
-    title: "Anggota",
-    url: "#"
-  },
-  {
-    title: "Pelanggaran & Sanksi",
-    url: "#"
-  },
-]
+const menus = [
+	{
+		title: "Dashboard",
+		url: "#",
+		icon: LayoutDashboard,
+	},
+	{
+		title: "Buku",
+		url: "#",
+		icon: Book,
+		subMenus: [
+			{
+				title: "Daftar Buku",
+				url: "#",
+			},
+			{
+				title: "Penulis",
+				url: "#",
+			},
+			{
+				title: "Reservasi Buku",
+				url: "#",
+			},
+			{
+				title: "Peminjaman Buku",
+				url: "#",
+			},
+			{
+				title: "Pelanggaran Peminjaman Buku",
+				url: "#",
+			},
+		],
+	},
+	{
+		title: "Pelanggaran & Sanksi",
+		url: "#",
+		icon: Scale,
+	},
+	{
+		title: "Anggota",
+		url: "#",
+		icon: Users,
+	},
+];
 
 function DashboardSidebar() {
-  const {
-    state,
-    open,
-    setOpen,
-    openMobile,
-    setOpenMobile,
-    isMobile,
-    toggleSidebar,
-  } = useSidebar()
+	const {
+		state,
+		open,
+		setOpen,
+		openMobile,
+		setOpenMobile,
+		isMobile,
+		toggleSidebar,
+	} = useSidebar();
 
-  return (
-    <Sidebar collapsible="icon" variant="inset" className="bg-blue-300">
-      <SidebarHeader>
-        <p className="py-2 px-4 border rounded text-center font-semibold ">
-          APP Logo
-        </p>
-      </SidebarHeader>
-      <SidebarContent>
-        <SidebarMenu>
-          {items.map((item) => (
-            <SidebarMenuItem key={item.title}>
-              <SidebarMenuButton asChild>
-                <a href={item.url}>
-                  <span>{item.title}</span>
-                </a>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          ))}
-        </SidebarMenu>
-      </SidebarContent>
-      <SidebarFooter>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton>User Name</SidebarMenuButton>
-          </SidebarMenuItem>
-          <SidebarMenuItem>
-            <SidebarMenuButton>Logout</SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarFooter>
-    </Sidebar>
-  )
+	const sidebarMenu = (
+		<SidebarMenu>
+			{menus.map((item) => {
+				let menuItem = null;
+
+				if (item["subMenus"]) {
+					menuItem = (
+						<Collapsible className="group/collapsible" key={item.title}>
+							<SidebarMenuItem>
+								<CollapsibleTrigger asChild>
+									<SidebarMenuButton>
+										<item.icon />
+										<span>{item.title}</span>
+										<ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+									</SidebarMenuButton>
+								</CollapsibleTrigger>
+								<CollapsibleContent>
+									<SidebarMenuSub>
+										{item.subMenus.map((sub) => (
+											<SidebarMenuSubItem key={sub.title}>
+												<SidebarMenuSubButton asChild>
+													<a href={sub.url} title={sub.title}>
+														<span>{sub.title}</span>
+													</a>
+												</SidebarMenuSubButton>
+											</SidebarMenuSubItem>
+										))}
+									</SidebarMenuSub>
+								</CollapsibleContent>
+							</SidebarMenuItem>
+						</Collapsible>
+					);
+				} else {
+					menuItem = (
+						<SidebarMenuItem key={item.title}>
+							<SidebarMenuButton asChild>
+								<a href={item.url} title={item.title}>
+									<item.icon />
+									<span>{item.title}</span>
+								</a>
+							</SidebarMenuButton>
+						</SidebarMenuItem>
+					);
+				}
+
+				return menuItem;
+			})}
+		</SidebarMenu>
+	);
+
+	return (
+		<Sidebar collapsible="icon">
+			<SidebarHeader>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuSubButton>
+							<SquareLibrary />
+							<span className="font-semibold text-xl">App Name</span>
+						</SidebarMenuSubButton>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarHeader>
+			<SidebarContent className="p-2">{sidebarMenu}</SidebarContent>
+			<SidebarFooter>
+				<SidebarMenu>
+					<SidebarMenuItem>
+						<SidebarMenuButton className="h-auto" asChild>
+							<a href="#">
+								<CircleUserRound />
+								<div className="grid">
+									<p className="font-semibold text-md">John Doe</p>
+									<p>Pustakawan</p>
+								</div>
+							</a>
+						</SidebarMenuButton>
+					</SidebarMenuItem>
+					<SidebarMenuItem>
+						<form>
+							<SidebarMenuButton
+								type="button"
+								className="h-auto cursor-pointer"
+							>
+								<LogOut /> <span className="">Logout</span>
+							</SidebarMenuButton>
+						</form>
+					</SidebarMenuItem>
+				</SidebarMenu>
+			</SidebarFooter>
+		</Sidebar>
+	);
 }
 
-export { DashboardSidebar }
+export { DashboardSidebar };
--- src/app/dashboard/components/dashboardTrigger.jsx
+++ src/app/dashboard/components/dashboardTrigger.jsx
@@ -1,45 +1,54 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
-import { useSidebar } from "@/components/ui/sidebar"
-import { AlignJustify, AlignJustifyIcon, PanelLeftCloseIcon, PanelLeftOpen, PanelLeftOpenIcon } from "lucide-react"
+import { Button } from "@/components/ui/button";
+import { useSidebar } from "@/components/ui/sidebar";
+import {
+	AlignJustify,
+	AlignJustifyIcon,
+	ChevronLeft,
+	ChevronRight,
+	PanelLeftCloseIcon,
+	PanelLeftOpen,
+	PanelLeftOpenIcon,
+} from "lucide-react";
 
 function DashboardTrigger({ className }) {
-  const { 
-    state,
-    open,
-    setOpen,
-    openMobile,
-    setOpenMobile,
-    isMobile,
-    toggleSidebar,
-  } = useSidebar()
+	const {
+		state,
+		open,
+		setOpen,
+		openMobile,
+		setOpenMobile,
+		isMobile,
+		toggleSidebar,
+	} = useSidebar();
 
-  let cssClasses = `size-10 absolute z-10`
-  let icon = null
+	let cssClasses = `size-7 rounded bg-sidebar text-dark absolute z-10 hover:bg-white hover:text-dark`;
+	let icon = null;
 
-  if (isMobile) {
-    cssClasses += ' top-2 right-0'
-    icon = <AlignJustify className="w-10" />
-  } else  {
-    cssClasses += ' top-4 left-[-1rem]'
-    icon = <PanelLeftOpen className="w-10" />
+	if (isMobile) {
+		cssClasses += " top-2 right-0";
+		icon = <AlignJustify className="w-10" />;
+	} else {
+		cssClasses += " top-3 left-[-1rem] hover:";
+		icon = <ChevronRight className="w-10" />;
 
-    if (state === "expanded") {
-      icon = <PanelLeftCloseIcon className="w-10" />
-    }
-  }
+		if (state === "expanded") {
+			cssClasses += " border-none shadow-none left-[-2.5rem]";
+			icon = <ChevronLeft className="w-10" />;
+		}
+	}
 
-  return (
-    <Button 
-      variant="ghost" 
-      size="icon" 
-      className={cssClasses + ` ${className}`} 
-      onClick={toggleSidebar}
-    >
-      {icon}
-    </Button>
-  )
+	return (
+		<Button
+			variant="default"
+			size="icon"
+			className={cssClasses + ` ${className}`}
+			onClick={toggleSidebar}
+		>
+			{icon}
+		</Button>
+	);
 }
 
-export { DashboardTrigger }
+export { DashboardTrigger };
