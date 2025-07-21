cc666189d210ae9b4055095986fd3ed0dc16ad73
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-07-21 13:22:58 +0700

    change bg color to white

--- src/app/dashboard/layout.jsx
+++ src/app/dashboard/layout.jsx
@@ -11,7 +11,7 @@ export default function DashboardLayout({ children }) {
         <section className="flex-none p-3 min-h-12 bg-gray-600 md:hidden">
           <h3 className="text-md font-semibold text-white">App Name</h3>
         </section>
-        <section className="flex-1 ps-3 pr-3 pt-3 bg-amber-100">
+        <section className="flex-1 ps-3 pr-3 pt-3">
           {children}
         </section>
       </main>
