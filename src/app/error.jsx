d07ea6e042c1a6cc5cfbf1a43b6f5cb8270e7562
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-07-21 17:16:25 +0700

    UPD: adding global route error page component

---
+++ src/app/error.jsx
@@ -0,0 +1,34 @@
+//TODO: styling error page
+
+/*
+  ---- Notes ----
+  - An error component can use the reset() function to prompt the user to attempt to recover from the error. When executed, the function will try to re-render the error boundary's contents. If successful, the fallback error component is replaced with the result of the re-render.
+
+*/
+
+'use client'
+
+import { Button } from "@/components/ui/button"
+import { useEffect } from "react"
+
+
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error)
+  }, [error])
+
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  )
+}
