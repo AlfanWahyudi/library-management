14406fa2c3d959a5c348f73c67c748cba2e245c4
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-08-01 16:30:04 +0700

    UPD: change empty tag on return page login component

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -49,55 +49,53 @@ export default function LoginPage() {
   }
 
   return (
-    <>
-      <main className="min-h-screen flex">
-        <section className="flex-1 flex flex-col m-3">
-          <section className="flex-none">
-            <Button>This is App Logo</Button>
-          </section>
-          <section className="flex-1 flex">
-            <div className="m-auto w-full md:max-w-96">
-              <h2 className="mb-10 font-bold text-xl text-center">Login to the app</h2>
-              <Alert className="mb-5" variant="destructive">
-                <AlertCircleIcon />
-                <AlertTitle>Username/Password salah</AlertTitle>
-              </Alert>
-              <form onSubmit={handleSubmit} action={action} noValidate>
-                <div className="flex flex-col gap-6">
-                  <InputControl 
-                    label="Username"
-                    name="username"
-                    id="username"
-                    type="text"
-                    value={usernameValue}
-                    onChange={handleUsernameChange}
-                    onBlur={handleUsernameBlur}
-                    hasError={didUsernameEdited && !usernameIsValid}
-                    errorMsg={usernameErrors}
-                  />
-                  <InputControl 
-                    label="Password"
-                    name="password"
-                    id="password"
-                    type="text"
-                    value={passwordValue}
-                    onChange={handlePasswordChange}
-                    onBlur={handlePasswordBlur}
-                    hasError={didPasswordEdited && !passwordIsValid}
-                    errorMsg={passwordErrors}
-                  />
-                  <Button type="submit">
-                    {isPending ? 'Submitted....' : 'Login' }
-                  </Button>
-                </div>
-              </form>
-            </div>
-          </section>
+    <main className="min-h-screen flex">
+      <section className="flex-1 flex flex-col m-3">
+        <section className="flex-none">
+          <Button>This is App Logo</Button>
         </section>
-        <section className="hidden lg:block lg:flex-1 lg:bg-amber-950">
-          <h2 className="text-4xl text-center font-bold text-white">Gambar di sini</h2>
+        <section className="flex-1 flex">
+          <div className="m-auto w-full md:max-w-96">
+            <h2 className="mb-10 font-bold text-xl text-center">Login to the app</h2>
+            <Alert className="mb-5" variant="destructive">
+              <AlertCircleIcon />
+              <AlertTitle>Username/Password salah</AlertTitle>
+            </Alert>
+            <form onSubmit={handleSubmit} action={action} noValidate>
+              <div className="flex flex-col gap-6">
+                <InputControl 
+                  label="Username"
+                  name="username"
+                  id="username"
+                  type="text"
+                  value={usernameValue}
+                  onChange={handleUsernameChange}
+                  onBlur={handleUsernameBlur}
+                  hasError={didUsernameEdited && !usernameIsValid}
+                  errorMsg={usernameErrors}
+                />
+                <InputControl 
+                  label="Password"
+                  name="password"
+                  id="password"
+                  type="text"
+                  value={passwordValue}
+                  onChange={handlePasswordChange}
+                  onBlur={handlePasswordBlur}
+                  hasError={didPasswordEdited && !passwordIsValid}
+                  errorMsg={passwordErrors}
+                />
+                <Button type="submit">
+                  {isPending ? 'Submitted....' : 'Login' }
+                </Button>
+              </div>
+            </form>
+          </div>
         </section>
-      </main>
-    </>
+      </section>
+      <section className="hidden lg:block lg:flex-1 lg:bg-amber-950">
+        <h2 className="text-4xl text-center font-bold text-white">Gambar di sini</h2>
+      </section>
+    </main>
   )
 }
