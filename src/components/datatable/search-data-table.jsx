94b346f494bee49ada6f27a1b6bda68e9a86e918
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-08-29 22:16:55 +0700

    UPD: searchDataTable component for now using state to handle input search

--- src/components/datatable/search-data-table.jsx
+++ src/components/datatable/search-data-table.jsx
@@ -1,12 +1,21 @@
+import { useInput } from "@/hooks/use-input";
 import { Input } from "../ui/input";
 
 export default function SearchDataTable({ searchingFieldItems, table }) {
+  const {
+    value,
+    handleInputChange
+  } = useInput(table.getState().globalFilter)
+
   const searchingFields = searchingFieldItems.map((item) => item.name.toLowerCase()).join(', ')
 
   return <Input 
     className="w-100" 
     placeholder={`Cari ${searchingFields}...`}
-    value={table.getState().globalFilter}
-    onChange={(e) => table.setGlobalFilter(String(e.target.value))}
+    value={value}
+    onChange={(e) => {
+      table.setGlobalFilter(e.target.value)
+      handleInputChange(e)
+    }}
   />
 }
