f53c69b49ce2dca42ebfbab7042f270125ebd476
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-10-10 10:54:11 +0700

    UPD: global-error page layout

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,4 +1,4 @@
-//TODO: styling error page
+//TODO: adding background image
 
 /*
   ---- Notes ----
@@ -11,24 +11,29 @@
 import { Button } from "@/components/ui/button"
 import { useEffect } from "react"
 
-
-export default function Error({ error, reset }) {
+export default function RootError({ error, reset }) {
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error)
   }, [error])
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <Button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </Button>
-    </div>
+    <html>
+      <body className="overflow-x-hidden max-w-screen">
+        <div className="flex flex-col items-center justify-center min-h-screen p-4 space-y-4 text-center">
+          <h2 className="text-4xl font-bold">Error Membuka Halaman</h2>
+          <p className="text-lg text-gray-600">Mohon untuk mencoba lagi kembali, bisa dengan menekan tombol refresh dibawah ini.</p>
+          <Button
+            variant='outline'
+            onClick={
+              // Attempt to recover by trying to re-render the segment
+              () => reset()
+            }
+          >
+            Refresh
+          </Button>
+        </div>
+      </body>
+    </html>
   )
 }
