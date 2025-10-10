8556393cdfc44a93f707603d66bf25039093fa90
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-10-10 11:05:05 +0700

    UPD: separate form login component

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,73 +1,9 @@
 'use client'
 
 import { Button } from "@/components/ui/button";
-import { login } from "@/app/actions/auth"
-import { useActionState, useEffect, useState } from "react";
-import { Alert, AlertTitle } from "@/components/ui/alert";
-import { AlertCircleIcon } from "lucide-react";
-import { loginSchema } from "@/lib/schemas/login-schema";
-import InputControl from "@/components/common/input/input-control";
-import { useInput } from "@/hooks/use-input";
-import { useValidateSpecificSchema } from "@/hooks/use-validate-specific-schema";
+import LoginForm from "@/components/specific/login/form";
 
-//TODO: cleaning code
 export default function LoginPage() {
-  const [state, action, isPending] = useActionState(login, undefined)
-  const [isSubmitted, setIsSubmitted] = useState(false)
-
-  // ---- username field ----
-  const {
-    value: usernameValue,
-    didEdit: didUsernameEdited,
-    handleInputBlur: handleUsernameBlur,
-    handleInputChange: handleUsernameChange,
-  } = useInput('')
-  
-  const {
-    isValid: usernameIsValid,
-    errors: usernameErrors,
-    setErrorsState: setErrorsUsername,
-  } = useValidateSpecificSchema({
-    schema: loginSchema.shape['username'],
-    value: usernameValue
-  })
-
-  const usernameHasError = (isSubmitted || didUsernameEdited) && !usernameIsValid
-
-  // ---- password field ----
-  const {
-    value: passwordValue,
-    didEdit: didPasswordEdited,
-    handleInputBlur: handlePasswordBlur,
-    handleInputChange: handlePasswordChange,
-  } = useInput('')
-
-  const {
-    isValid: passwordIsValid,
-    errors: passwordErrors,
-    setErrorsState: setErrorsPassword,
-  } = useValidateSpecificSchema({
-    schema: loginSchema.shape['password'],
-    value: passwordValue
-  })
-
-  const passwordHasError = (isSubmitted || didPasswordEdited) && !passwordIsValid
-
-  useEffect(() => {
-    if (state) {
-      setErrorsUsername(state.username)
-      setErrorsPassword(state.password)
-    }
-  }, [state])
-
-  function handleSubmit(e) {
-    setIsSubmitted(true)
-
-    if (!usernameIsValid || !passwordIsValid) {
-      e.preventDefault()
-    }
-  }
-
   return (
     <main className="min-h-screen flex">
       <section className="flex-1 flex flex-col m-3">
@@ -75,46 +11,7 @@ export default function LoginPage() {
           <Button>This is App Logo</Button>
         </section>
         <section className="flex-1 flex">
-          <div className="m-auto w-full md:max-w-96">
-            <h2 className="mb-10 font-bold text-xl text-center">Login to the app</h2>
-            {
-              state?.form && (
-                <Alert className="mb-5" variant="destructive">
-                  <AlertCircleIcon />
-                  <AlertTitle>{state?.form}</AlertTitle>
-                </Alert>
-              )
-            }
-            <form onSubmit={handleSubmit} action={action} noValidate>
-              <div className="flex flex-col gap-6">
-                <InputControl 
-                  label="Username"
-                  name="username"
-                  id="username"
-                  type="text"
-                  value={usernameValue}
-                  onChange={handleUsernameChange}
-                  onBlur={handleUsernameBlur}
-                  hasError={usernameHasError}
-                  errorMsg={usernameErrors}
-                />
-                <InputControl 
-                  label="Password"
-                  name="password"
-                  id="password"
-                  type="password"
-                  value={passwordValue}
-                  onChange={handlePasswordChange}
-                  onBlur={handlePasswordBlur}
-                  hasError={passwordHasError}
-                  errorMsg={passwordErrors}
-                />
-                <Button type="submit">
-                  {isPending ? 'Submitted....' : 'Login' }
-                </Button>
-              </div>
-            </form>
-          </div>
+          <LoginForm />
         </section>
       </section>
       <section className="hidden lg:block lg:flex-1 lg:bg-amber-950">
