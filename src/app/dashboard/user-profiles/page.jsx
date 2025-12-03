7549380b73fa8436915c17ce937bdfaf1dde8ebf
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2025-12-03 16:10:42 +0700

    UPD: placeholder for updating username

--- src/app/dashboard/user-profiles/page.jsx
+++ src/app/dashboard/user-profiles/page.jsx
@@ -1,6 +1,7 @@
 import ContentHead from "@/components/specific/content-head";
 import UserProfileBreadcrumb from "@/components/specific/user-profiles/breadcrumb";
 import UserProfileForm from "@/components/specific/user-profiles/form";
+import { Button } from "@/components/ui/button";
 import SessionDAL from "@/lib/dal/session-dal";
 import UserService from "@/lib/services/user-service";
 
@@ -12,13 +13,14 @@ export default async function userProfilePage() {
   }
 
   const user = await UserService.getById(session.userId)
-  console.log(user)
 
   return (
     <section className="">
       <UserProfileBreadcrumb />
       <h1 className="sr-only">User Profile Page</h1>
       <ContentHead pageTitle='Profile'>
+        {/* TODO: jalankan func ganti username */}
+        <Button>Mengganti Username (Belum Selesai)</Button>
       </ContentHead>
 
       <UserProfileForm 
