0d90ea3d9862071a004cff1a8d0d74234dfbc6d6
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2026-01-22 16:42:01 +0700

    upd: styling flexbox of content-head section

--- src/components/specific/content-head.jsx
+++ src/components/specific/content-head.jsx
@@ -3,11 +3,11 @@
 export default function ContentHead({ pageTitle, children }) {
   return (
     <section className="grid gap-1 mb-3">
-      <section className="md:flex md:flex-row-reverse md:align-middle">
-        <div className='mb-3 md:m-0 flex gap-3'>
+      <section className="flex flex-wrap align-middle justify-between gap-y-6">
+        <h2 className="text-2xl font-semibold ">{pageTitle}</h2>
+        <div className='flex flex-wrap gap-3'>
           {children}
         </div>
-        <h2 className="text-2xl font-semibold md:flex-1">{pageTitle}</h2>
       </section>
     </section>
   )
