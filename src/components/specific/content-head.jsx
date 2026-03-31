eab2c25b58acb92576960b37575cebd662dffd57
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2026-03-31 14:51:44 +0700

    upd: checking children is exist or not for content-head component

--- src/components/specific/content-head.jsx
+++ src/components/specific/content-head.jsx
@@ -1,13 +1,17 @@
 "use client"
 
+import { Children } from "react"
+
 export default function ContentHead({ pageTitle, children }) {
   return (
     <section className="grid gap-1 mb-3">
       <section className="flex flex-wrap align-middle justify-between gap-y-6">
         <h2 className="text-2xl font-semibold ">{pageTitle}</h2>
-        <div className='flex flex-wrap gap-3'>
-          {children}
-        </div>
+        {Children.count(children) > 0 && (
+          <div className='flex flex-wrap gap-3'>
+            {children}
+          </div>
+        )}
       </section>
     </section>
   )
