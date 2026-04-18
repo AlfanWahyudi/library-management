f0632040c0a0f958dce1d28b1a76216d1b4ba448
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2026-04-18 20:36:48 +0700

    upd: make header sticky on top of page when scrolling

--- src/components/specific/header.jsx
+++ src/components/specific/header.jsx
@@ -7,7 +7,7 @@ import { Separator } from "@radix-ui/react-dropdown-menu"
 // TODO: make it sticky, cannot hide while scrolling
 export default function DashHeader() {
   return (
-    <header className="flex min-h-14 max-h-20 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    <header className="flex min-h-14 max-h-20 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 bg-white sticky top-0 z-20 shadow-sm">
       <div className="flex items-center gap-1 px-8">
         <SidebarTrigger className="-ml-1" />
         <Separator
