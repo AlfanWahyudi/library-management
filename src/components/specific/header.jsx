66ff380d3a90c161bb17cf2b47e8c659841e9e9f
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2026-04-20 13:13:12 +0700

    remove unused todo comment

--- src/components/specific/header.jsx
+++ src/components/specific/header.jsx
@@ -4,7 +4,6 @@ import PrimaryBreadcrumb from "@/components/common/primary-breadcrumb"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import { Separator } from "@radix-ui/react-dropdown-menu"
 
-// TODO: make it sticky, cannot hide while scrolling
 export default function DashHeader() {
   return (
     <header className="flex min-h-14 max-h-20 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 bg-white sticky top-0 z-20 shadow-sm">
