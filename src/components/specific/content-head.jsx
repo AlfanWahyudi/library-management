0db70c32d9af8d10a632c25e19fb0dad85be6876
Alfan Wahyudi <alfan.wahyudi98@gmail.com>
2026-04-20 13:17:27 +0700

    upd: styling layout of dashboard for header and page title section

--- src/components/specific/content-head.jsx
+++ src/components/specific/content-head.jsx
@@ -1,12 +1,13 @@
 "use client"
 
 import { Children } from "react"
+import TitlePage from "../common/title-page"
 
 export default function ContentHead({ pageTitle, children }) {
   return (
     <section className="grid gap-1 mb-3">
       <section className="flex flex-wrap align-middle justify-between gap-y-6">
-        <h2 className="text-2xl font-semibold ">{pageTitle}</h2>
+        <TitlePage className="mb-0!">{pageTitle}</TitlePage>
         {Children.count(children) > 0 && (
           <div className='flex flex-wrap gap-3'>
             {children}
