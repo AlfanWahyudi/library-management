d45417c7c94ba167bbf3ed2cb886b04b18462355
fan <alfan.wahyudi98@gmail.com>
2026-07-31 14:41:01 +0700

    upd: remove config to add min max fraction for formatting number utils

--- src/lib/utils/number.js
+++ src/lib/utils/number.js
@@ -1,10 +1,7 @@
 import { LOCALE } from "../constants/locale";
 
 const formatNumber = (value) => {
-  return new Intl.NumberFormat(LOCALE.default, {
-    minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  }).format(value);
+  return new Intl.NumberFormat(LOCALE.default).format(value);
 }
 
 export {
