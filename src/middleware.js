013e3f486069458877c4a38ce137316f02fa4cdd
fan <alfan.wahyudi98@gmail.com>
2026-08-08 09:09:22 +0700

    upd: middleware matcher regex

--- src/middleware.js
+++ src/middleware.js
@@ -2,24 +2,6 @@ import { NextResponse } from "next/server";
 import SessionDAL from "./lib/dal/session-dal";
 import { ROUTE, ROUTE_AUTHORIZE } from "./lib/constants/route";
 
-/*
-
--- daftar pengecekan di middleware 
-
----
-cek apakah user berhasil authenticate,
-jika tidak redirect ke halaman login dgn memberikan alert "user belum login (sesuaikan kalimatnya)",
-jika berhasil masuk ke halaman dashboard dgn memberikan ucapan selamat "berhasil login (sesuaikan kalimatnya)"
----
-
-=================================================
--- daftar routes that excluded tanpa melewati middleware
-// TODO: tentukan route apa untuk di exclude
-"api, _next/static, _next/image, .json, .png, dll"
-
-
-*/
-
 export default async function middleware(req) {
   const { publics: publicRoutes, protected: protectedRoutes } = ROUTE_AUTHORIZE
   
@@ -46,8 +28,7 @@ export default async function middleware(req) {
   return NextResponse.next()
 }
 
-// Routes Middleware should not run on
 export const config = {
-  // TODO: perbaiki lagi macher nya, karena file .json tetep kena masuk middleware juga
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  // Routes Middleware should not run on
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 }
