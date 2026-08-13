c18c5b9607fc2f1099e21e5b9b4063a4ca09c010
fan <alfan.wahyudi98@gmail.com>
2026-08-13 09:27:07 +0700

    rmv: todo comment

--- src/app/api/books/[id]/can-delete/route.js
+++ src/app/api/books/[id]/can-delete/route.js
@@ -5,7 +5,6 @@ import { generateErrorHttpRes } from "@/lib/utils/http"
 import { NextResponse } from "next/server"
 
 export async function GET(req, { params }) {
-  // TODO: permission intuk route handler ini tambah di DB
   try {
     await BookRouteAuth.verifyDataCanDeleted()
 
