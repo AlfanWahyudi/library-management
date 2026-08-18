8812101c691b2a2792a0f99772c730696dd0d7f1
fan <alfan.wahyudi98@gmail.com>
2026-08-18 11:08:12 +0700

    upd: clickable image on login to resource image

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import LoginForm from "@/components/specific/login/form";
 import Image from "next/image";
 import libraryImage from "../../public/images/library.jpg"
+import Link from "next/link";
 
 export default function LoginPage() {
   return (
@@ -11,17 +12,19 @@ export default function LoginPage() {
         </section>
       </section>
       <section className="hidden lg:block lg:flex-1 relative">
-        <Image 
-          fill
-          src={libraryImage} 
-          alt="library background image"
-          quality={70}
-          style={{
-            objectFit: 'cover',
-            borderRadius: '.8rem',
-            borderWidth: 1,
-          }}
-        />
+        <Link title="reference: Unsplash" target="_blank" href="https://unsplash.com/photos/a-long-row-of-books-on-shelves-in-a-library--7EqDr9UE_M" rel="noopener noreferrer">
+          <Image 
+            fill
+            src={libraryImage} 
+            alt="library Image, reference from unsplash.com"
+            quality={70}
+            style={{
+              objectFit: 'cover',
+              borderRadius: '.8rem',
+              borderWidth: 1,
+            }}
+          />
+        </Link>
       </section>
     </main>
   )
